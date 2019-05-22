a5846a7c74bdad1efdccb82ac09425fc262b000e
SchmidMaxi <maximilian.schmid@thi.de>
2019-05-22 15:44:40 +0200

    Vorbereitung Movement Box

--- Test_Umgebung/Window_Testumgebung.cs
+++ Test_Umgebung/Window_Testumgebung.cs
@@ -17,30 +17,24 @@ namespace Test_Umgebung
 {
     public partial class Window_Testumgebung : Form
     {
-<<<<<<< HEAD
+
         PowerSupply_HMP mySupply;
         I_TEC_Controller myTEC;
         
-
-=======
-        PowerSupply_HMP mySupply = new PowerSupply_HMP();
         ReadBox_Movement myMovement;
->>>>>>> 635ae0261fbcdedc98392f70a69617626c53d66d
+
 
         public Window_Testumgebung()
         {
             InitializeComponent();
 
-<<<<<<< HEAD
             mySupply = new PowerSupply_HMP(this, 10, 10);
 
             myTEC = new Meerstetter_4fach(this, 10, 100);
             
-=======
-            PowerSupply_HMP mySupply = new PowerSupply_HMP(this, 10, 10);
 
             myMovement = new ReadBox_Movement(this, 10, 100);
->>>>>>> 635ae0261fbcdedc98392f70a69617626c53d66d
+
         }
     }
 }
---
+++ Test_Umgebung2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Power_Supply_HamegHMP;
+using Read_Coordinates;
+
+using TEC_Controller;
+
+namespace Test_Umgebung2
+{
+    public partial class Form1 : Form
+    {
+        PowerSupply_HMP mySupply;
+        I_TEC_Controller myTEC;
+
+        ReadBox_Movement myMovement;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializeComponent();
+
+            mySupply = new PowerSupply_HMP(this, 10, 10);
+
+            myTEC = new Meerstetter_4fach(this, 10, 100);
+
+
+            myMovement = new ReadBox_Movement(this, 10, 100);
+        }
+    }
+}
